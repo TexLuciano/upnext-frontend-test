26a226864e3586154f0ec9ee864faafe9756f179
TexLuciano <lucianomartinello15@gmail.com>
2023-03-21 21:54:32 -0300

    test final bugs

--- script.js
+++ script.js
@@ -1,114 +1,106 @@
-let plants = []
-
-fetch('plants.json')
-  .then((response) => response.json())
-  .then((data) => plants = data);
-
-
+const listPlants = document.querySelector('#plant-list');
 const sunSelect = document.querySelector('#sun');
 const waterSelect = document.querySelector('#water');
 const petsSelect = document.querySelector('#pets');
+const noResult = document.querySelector('#no-result');
+const pickPlant = document.querySelector('#pickPlant');
 
+let plants = [];
+let plantsFiltered = [];
+let isFiltered = false;
 
-sunSelect.addEventListener('change', filterPlants);
-waterSelect.addEventListener('change', filterPlants);
-petsSelect.addEventListener('change', filterPlants);
+fetch('./plants.json')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    plants = data;
+    render(data);
+  })
+  .catch((error) => {
+    console.error(' fetch unsuccessfully:', error);
+  });
 
+function filterPlants(e, type) {
+  if (isFiltered) listPlants.innerHTML = '';
 
-const filteredPlants = plants.filter(plant => {
-  if (sunlightValue && plant.sun !== sunlightValue) {
-    return false;
+  let filterPlants = [];
+
+  const updateValue = {
+    sun: () => {
+      if (waterSelect.value !== '' || petsSelect.value !== '') {
+        waterSelect.value = '';
+        petsSelect.value = '';
+      }
+      filterPlants = plants.filter((plant) => plant.sun === e.target.value);
+    },
+
+    water: () => {
+      if (sunSelect.value !== '' || petsSelect.value !== '') {
+        sunSelect.value = '';
+        petsSelect.value = '';
+      }
+
+      filterPlants = plants.filter((plant) => plant.water === e.target.value);
+    },
+
+    pets: () => {
+      if (waterSelect.value !== '' || sunSelect.value !== '') {
+        waterSelect.value = '';
+        sunSelect.value = '';
+      }
+      console.log(petsSelect.value);
+
+      filterPlants = plants.filter(
+        (plant) => String(plant.toxicity) !== e.target.value,
+      );
+      
+      if (petsSelect.value == '') {
+        filterPlants = [];
+      }
+    },
+  };
+
+  updateValue[type]();
+
+  console.log(filterPlants);
+
+  if (filterPlants.length === 0) {
+    pickPlant.classList.remove('ative');
+    noResult.classList.add('ative-result');
+  } else {
+    pickPlant.classList.add('ative');
+    noResult.classList.remove('ative-result');
   }
-  if (waterValue && plant.water !== waterValue) {
-    return false;
-  }
-  if (petsValue === 'true' && plant.toxicity !== false) {
-    return false;
-  }
-  if (petsValue === 'false' && plant.toxicity !== true) {
-    return false;
-  }
-  return true;
-});
 
+  render(filterPlants);
 
+  return (isFiltered = true);
+}
 
+function render(plants) {
+  listPlants.innerHTML = '';
 
-const sunValue = sunlightSelect.value;
-const waterValue = waterSelect.value;
-const petsValue = petsSelect.value;
+  plants.forEach((plant) => {
+    const itemList = document.createElement('li');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    const span = document.createElement('span');
 
-console.log(sunValue);
+    img.src = plant.url;
+    img.alt = plant.name;
+    p.innerText = plant.name;
+    span.innerText = `$${plant.price}`;
 
+    Promise.all(
+      [itemList.appendChild(img)],
+      [itemList.appendChild(p)],
+      [itemList.appendChild(span)],
+    );
 
+    listPlants.appendChild(itemList);
+  });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const containerPlants = document.getElementById('plant-list')
-// let listPlants = []
-
-//  const render = async ()=>{
-//   const dadosResponse = fetch('plants.json')
-//   const data = await( await dadosResponse).json()
-//   listPlants = data
-
-
-//   for (let i = 0; i < listPlants.length; i++){
-//     const li = document.createElement('li');
-//     const img = document.createElement('img');
-//     const span = document.createElement('span');
-  
-//     img.src = listPlants[i].url;
-//     img.alt = listPlants[i].name;
-//     span.textContent= listPlants[i].name
-  
-//     li.appendChild(img)
-//     li.appendChild(span)
-//     containerPlants.appendChild(li)
-//   }
-   
-  
-//  }
-
-//  render()
-
-
-
-
-
-// fetch('plants.json')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+sunSelect.addEventListener('change', (e) => filterPlants(e, 'sun'));
+waterSelect.addEventListener('change', (e) => filterPlants(e, 'water'));
+petsSelect.addEventListener('change', (e) => filterPlants(e, 'pets'));
